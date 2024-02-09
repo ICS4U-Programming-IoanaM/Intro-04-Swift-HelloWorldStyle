e4553a7fc94e277946d00e03e23c483e390a7618
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-09 22:59:06 +0000

    Completion of HelloWorldStyle.swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,11 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Ioana Marinescu
+//  Created on 2024/02/09
+//  Version 1.0
+//  Copyright (c) 2024 Ioana Marinescu. All rights reserved.
+//
+//  Explain what the program does.
+
+print("Hello World, in style!")
